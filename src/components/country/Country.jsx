efb7db966afc42692e13bb3c47adc80c9f1249be
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-03-29 13:24:25 +0600

    Contents updated

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,15 +1,15 @@
-export default function Country({country}){
+export default function Country({ country }) {
     // console.log(country)
-    const {name, flags, independent, languages, capital} = country;
+    const { name, flags, independent, languages, capital } = country;
     // console.log(languages)
     return (
         <div className="card">
+            <img className="flag-img" src={flags.png} alt={flags.alt} />
             <h2> {name.common} </h2>
-            <img src={flags.png} alt={flags.alt} />
             <div className="country-bio">
                 <p> Language: {languages && Object.values(languages)[0]} </p>
                 <p> Capital: {capital} </p>
-                <p> Independent: {independent ? "Independent Country" : "Not independent" }  </p>
+                <p> Independent: {independent ? "Independent Country" : "Not independent"}  </p>
             </div>
         </div>
     )
